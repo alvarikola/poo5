770a91c0a1b4b4ffa10da455302cb86cc354e0c5
alvarikola <alvaroe484@gmail.com>
2024-05-27 13:07:31 +0100

    completado tes_jugador

--- prueba28/tests/test_jugador.py
+++ prueba28/tests/test_jugador.py
@@ -1,3 +1,11 @@
 import pytest
 
-from jugador import jugador
+from prueba28.jugador.jugador import Jugador
+
+@pytest.fixture
+def jugador():
+    return Jugador("William", "Cricket")
+
+
+def test_read(jugador):
+    jugador.read() == "William,Cricket"
