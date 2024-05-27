2a1a835a3535757169a7316fed9e502989091636
alvarikola <148550896+alvarikola@users.noreply.github.com>
2024-05-27 21:23:32 +0100

    terminado test_persona.py

--- prueba28/tests/test_persona.py
+++ prueba28/tests/test_persona.py
@@ -11,3 +11,17 @@ def test_create(persona):
     assert persona.read() == "William"
 
 
+def test_read(persona):
+    assert persona.read() == "William"
+
+
+def test_update(persona):
+    persona.update("Alvaro")
+    assert persona.read() == "Alvaro"
+
+
+def test_delete(persona):
+    persona.delete()
+    assert persona.read() == None
+
+
