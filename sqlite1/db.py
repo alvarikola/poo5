b8933805c0bb37972136d4271338f49a7808e132
alvarikola <alvaroe484@gmail.com>
2024-06-03 13:07:59 +0100

    primera clase de programacion con base de datos

---
+++ sqlite1/db.py
@@ -0,0 +1,11 @@
+import sqlite3
+
+class Db:
+    @staticmethod
+    def conectar(name:str)->sqlite3.Connection:
+        return sqlite3.connect(name, isolation_level=None)
+
+
+    @staticmethod
+    def close(con:sqlite3.Connection):
+        con.close()
